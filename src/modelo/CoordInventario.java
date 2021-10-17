f4064c9cedb1763af70c3a36755d56cb2ccda73c
davidvaldh <ds.valderrama@uniandes.edu.co>
2021-10-17 08:45:12 -0500

    Sistema Inventario - Lógica terminada (falta interfaz))

---
+++ src/modelo/CoordInventario.java
@@ -0,0 +1,40 @@
+package modelo;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CoordInventario {
+
+	private AdminProductos adminProductos;
+	
+	public CoordInventario() {
+		adminProductos = new AdminProductos();
+	}
+	
+	public void cargarLotes(String nombreArchivo) throws IOException {
+		adminProductos.cargarLotes(nombreArchivo);
+	}
+	
+	public Producto consultarInfoProducto(int codigo) {
+		return adminProductos.consultarProducto(codigo);
+	}
+	
+	public void eliminarLotesVencidos(int dia, int mes, int año) throws FileNotFoundException, IOException {
+		Fecha fechaActual = new Fecha(dia, mes, año);
+		adminProductos.eliminarLotesVencidos(fechaActual);
+	}
+	
+	public float consultarDesempeñoFin(int codigo) {
+		Producto producto =  adminProductos.consultarProducto(codigo);
+		return producto.getGananciaVenta();
+	}
+	
+	public void cargarProductos() throws FileNotFoundException, ClassNotFoundException, IOException {
+		adminProductos.cargarProductos();
+	}
+		
+	public static void main(String[] args) throws IOException {
+		CoordInventario inv = new CoordInventario();
+		inv.cargarLotes("C:\\Users\\Lenovo\\eclipse-workspace\\Proyecto1\\EjemploArchInventario.csv");
+	}
+
+}
---
+++ src/modelo/Producto.java
@@ -0,0 +1,99 @@
+package modelo;
+
+import java.util.*;
+import java.io.*;
+
+public class Producto implements Serializable {
+	private int codigo;
+	private String nombre;
+	private float precioVenta;
+	private float gananciaVenta;
+	private boolean empacado;
+	private ArrayList<Lote> lotes;
+	private ArrayList<String> categorias;
+	
+	public Producto(int codigo, String nombre, boolean empacado) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.empacado = empacado;
+		lotes = new ArrayList<Lote>();
+		categorias =  new ArrayList<String>();
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(float precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public float getGananciaVenta() {
+		return gananciaVenta;
+	}
+
+	public void setGananciaVenta(float gananciaVenta) {
+		this.gananciaVenta = gananciaVenta;
+	}
+
+	public void addLote(Lote lote) {
+		lotes.add(lote);
+	}
+	
+	public void addCategoria(String nombreCategoria) {
+		categorias.add(nombreCategoria);
+	}
+	
+	public boolean eliminarLotesVencidos(Fecha fechaActual) {
+		boolean retorno = false;
+		for (Lote lote: lotes) {
+			Fecha fechaVencimiento = lote.getFechaVencimiento();
+			if (fechaVencimiento.compFecha(fechaVencimiento) == true) {
+				lotes.remove(lote);
+				retorno = true;
+				}
+			}
+		return retorno;
+	}
+	
+	public float contarCantidad() {
+		float cantidad = 0;
+		for (Lote lote : lotes) {
+			cantidad += lote.getCantidad();
+		}
+		return cantidad;
+	}
+	
+	public void restarCantidad(float cantidad) {
+		while (cantidad > 0) {
+			for (Lote lote : lotes) {
+				if (lote.getCantidad() > cantidad) {
+					lote.setCantidad(lote.getCantidad() - cantidad);
+					gananciaVenta += (precioVenta - lote.getPrecioCompra()) * cantidad;
+					cantidad = 0;
+					break;
+					
+				}
+				else if (lote.getCantidad() <= cantidad)
+					gananciaVenta += (precioVenta - lote.getPrecioCompra()) * lote.getCantidad();
+					cantidad -= lote.getCantidad();
+					lotes.remove(lote);
+			}
+		}
+		
+	}
+		
+
+}
